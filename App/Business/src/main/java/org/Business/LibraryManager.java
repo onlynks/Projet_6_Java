12a6ec0d720c5b284e2cf126af6bb91934533702
onlynks <nicogar12@gmail.com>
2018-12-21 02:25:12 +0100

    structure creation: first stop

---
+++ App/Business/src/main/java/org/Business/LibraryManager.java
@@ -0,0 +1,22 @@
+package org.Business;
+
+import org.beans.Book;
+
+public class LibraryManager {
+
+	public Book getBook(String title) {
+		
+	}
+	
+	public List<Book> getBookList(){
+		
+	}
+	
+	public void updateBook(String title) {
+		
+	}
+	
+	public void deleteBook(String title) {
+		
+	}
+}
