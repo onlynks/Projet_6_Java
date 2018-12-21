dc81455756434232881d2e2ea5591a5a219ebf03
onlynks <nicogar12@gmail.com>
2018-12-22 00:49:50 +0100

    groupId changed

---
+++ WebService/Business/src/main/java/com/projet3/webservice/App.java
@@ -0,0 +1,13 @@
+package com.projet3.webservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
