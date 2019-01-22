21c177697fb072273221ec040ebf1bce15cde032
onlynks <nicogar12@gmail.com>
2019-01-22 16:14:43 +0100

    checkpoint

--- WebApp/App/src/main/java/com/projet3/webapp/BookController.java
+++ WebApp/App/src/main/java/com/projet3/webapp/BookController.java
@@ -1,8 +1,11 @@
 package com.projet3.webapp;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.projet3.webapp.Model.Book;
@@ -10,15 +13,28 @@ import com.projet3.webapp.Model.Book;
 @Controller
 public class BookController extends AbstractBookController {	
 	
+	@RequestMapping(value= {"/", "/home"})
+	public String getHomePage() {
+		return "index";
+	}	
+
 	@GetMapping("/book")
 	public String getBook(@RequestParam(name="title", required=false) String title, Model model) {
 		//model.addAttribute("title", title);
 		Book book = bookManager.getBook("Tintin");
 		String description = book.getDescription();
 		model.addAttribute("description", description);
-		return "bookForm";
-
-		
+		return "bookForm";		
 	}
+	
+	@GetMapping("/bookList")
+	public String getBookList(Model model) {
+		List<Book> bookList = bookManager.getBookList();
+		model.addAttribute("bookList", bookList);
+		
+		return "bookList";
+	}
+	
+	
 
 }
--- WebApp/App/src/main/webapp/WEB-INF/jsp/bookForm.jsp
+++ WebApp/App/src/main/webapp/WEB-INF/jsp/bookForm.jsp
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
+<%@ include file="head.jsp" %>
+<%@ include file="menu.jsp" %>
+
 <body>
 Ca marche
 ${description }
